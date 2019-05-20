09b409542e48bfd754f176012c369e7025869723
Aleksandr <hoblex@gmail.com>
2019-05-20 16:24:23 +0300

    a

--- test.js
+++ test.js
@@ -14,17 +14,19 @@ console.log(x2);
     // Чему равна сумма [ ] + 1 + 2?
 console.log([ ] + 1 + 2);
     // Что выведет этот код: alert( "1"[0] )?
-console.log("1"[0]);
+console.log("21"[1]);
     // Чему равно 2 && 1 && null && 0 && undefined ?
 console.log(2 && 1 && null && 0 && undefined );
 
     // Есть ли разница между выражениями? !!( a && b ) и (a && b)?
-console.log("Есть ли разница между выражениями? !!( a && b ) и (a && b)" + " " + (true&&false));
+let a = true;
+let b = false;
+console.log("!!( a && b ) = " + !!( a && b ) + " ( a && b ) = " +  " (a && b)" + " " + (true&&false));
     // Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
 console.log(null || 2 && 3 || 4 );
     // a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
-let a = [1, 2, 3], b = [1, 2, 3];
-console.log(a === b);
+let a1 = [1, 2, 3], b1 = [1, 2, 3];
+console.log(a1 === b1);
 // Что выведет этот код: alert( +"Infinity" ); ?
 console.log(+"Infinity");
 // Верно ли сравнение: "Ёжик" > "яблоко"?
